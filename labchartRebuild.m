61ed743c9fb84af1a95a26500bb4615b9fc47ba7
doguetv <48283023+doguetv@users.noreply.github.com>
2019-03-06 13:14:18 +0100

    Add files via upload

---
+++ labchartRebuild.m
@@ -0,0 +1,33 @@
+%% Comments
+%Function to rebuild data from Labchar Matlab Export
+
+%Author: V. Doguet 24/05/2018
+%% Function
+function dataStruct = labchartRebuild(data, datastart, dataend, catMethod)
+
+%Varargin
+if nargin < 3
+    catMethod = 'continue';
+end
+
+switch catMethod
+    case 'continue'
+        %Allocate
+        dataStruct = cell(1, length(datastart(:,1)));
+        %Concatenate when there's stops in file
+        for i = 1:length(datastart(1, :))
+            for j = 1:length(datastart(:, 1))
+                dataStruct{j} = cat(1, dataStruct{j}, data(datastart(j,i):dataend(j,i))');
+            end
+        end
+    case 'split'
+        %Allocate
+        dataStruct = cell(length(datastart(1,:)), length(datastart(:,1)));
+        %Store in different cells when there's stops in file
+        for i = 1:length(datastart(1, :))
+            for j = 1:length(datastart(:, 1))
+                dataStruct{i, j} = data(datastart(j,i):dataend(j,i))';
+            end
+        end
+end
+
